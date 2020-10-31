e1d82bc990d4197749563768079648a414d10bb0
Roman Muzafarov <muzafarovroma@list.ru>
2020-10-31 17:35:21 +0500

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'; 
+import classNames from 'classnames'; 
+
+
+function Button({onClick, className, outline, children}) {
+    return (
+        <button 
+        onClick={onClick} 
+
+        className={classNames('button', className, {
+            'button--outline': outline,
+        })}>
+            {children}
+        </button>
+    );
+}
+
+export default Button; 
